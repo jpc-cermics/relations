3263c71c59172ae61ae0be65c6520dde4fde6da2
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2025-10-10 06:02:24 +0200

    mono.v

--- mono.v
+++ mono.v
@@ -2432,7 +2432,7 @@ Notation dec x := (sumbool_of_bool x).
 
 Section walk.
   (** * How to encode in one function the two functions obtained above *)
-  Variables (T:choiceType) (f: nat -> T).
+  Variables (T:choiceType).
   
   Equations? decode_aux (i row : nat) (p : nat -> nat) : nat* nat  by wf i lt :=
     decode_aux n row p with dec  ((p row).+1 <= n) => {
@@ -2461,53 +2461,45 @@ Section walk.
   Definition encode (g : nat -> seq T) (row col : nat) : nat :=
     (prefix_sum g row + col)%N.
 
-  Variables (a1 b1 c1 d1 e1 f1 g1 h1 i1 j1 k1 l1 m1 :T).
+  Definition val (f: nat -> T) (g : nat -> seq T) n := 
+    let (p,q):= decode g n in 
+    match q with
+    | 0 => f p
+    | S q => nth (f 0) (g p) q
+    end.
   
-  Definition G n := 
+  Variables (a1 b1 c1 d1 e1 f1 g1 h1 i1 j1 k1 l1 m1 :T).
+  Definition L:= [:: a1; b1; c1; d1; e1; f1; g1; h1; i1; j1; k1; l1; m1].
+  Definition g n := 
     match n with 
     | 0 => [:: b1 ; c1 ; d1]
     | 1 => [:: f1 ; g1]
-    | _ => [:: i1]
+    | 2 => [::]
+    | _ => [:: j1]
     end.
-
-  Definition F n := 
+  
+  Definition f n := 
     match n with 
     | 0 => a1
     | 1 => e1
-    | _ => h1
+    | 2 => h1
+    | 3 => i1
+    |_  => k1
     end.
   
-  Compute decode G 0.
-  Compute decode G 1.
-  Compute decode G 2.
-  Compute decode G 3.
-  Compute decode G 4.
-  
-  Definition Val n p := 
-    match p with
-    | 0 => F n
-    | S p => nth (F 0) (G n) p
-    end.
-  
-  Compute Val 0 0.
-  Compute Val 0 1.
-  Compute Val 0 2.
-  Compute Val 0 3.
-  Compute Val 1 0.
-  Compute Val 1 1.
+  (* should give a1 b1 c1 d1 e1 f1 g1 h1 i1 j1 *)
+  Compute ((val f g  0) =   (nth m1 L 1)).
+  Compute ((val f g  1) =   (nth m1 L 1)).
+  Compute ((val f g  2) =   (nth m1 L 2)).
+  Compute ((val f g  3) =   (nth m1 L 3)).
+  Compute ((val f g  4) =   (nth m1 L 4)).
+  Compute ((val f g  5) =   (nth m1 L 5)).
+  Compute ((val f g  6) =   (nth m1 L 6)).
+  Compute ((val f g  7) =   (nth m1 L 7)).
+  Compute ((val f g  8) =   (nth m1 L 8)).
+  Compute ((val f g  9) =   (nth m1 L 9)).
+  Compute ((val f g  10) =  (nth m1 L 10)).
 
-  Definition val n := 
-    let (p,q):= decode G n in Val p q.
-  
-  Compute val 0.
-  Compute val 1.
-  Compute val 2.
-  Compute val 3.
-  Compute val 4.
-  Compute val 5.
-  Compute val 6.
-  Compute val 7.
-  Compute val 8.
 
 End walk.
 
